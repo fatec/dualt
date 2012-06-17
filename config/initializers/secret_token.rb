e50b240b7e8696c033a0ebcf08cb0af74cf26d0e
Stéphane Morand <morands@greatmansarovar.fritz.box>
2012-06-17 13:42:13 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DualtRails3BootstrapDeviseCancan::Application.config.secret_token = '5196eb9e5cf19318e88151cb9e33da75ead919c944304f160c2490c31ccb2adc0d1d92781b50ed6a334642316c838c65aee098e8dec8b17466933e745f2fe321'
