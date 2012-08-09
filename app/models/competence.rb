b9abd47532c0926e0e585549312692a36ba855b9
Stéphane Morand <Stephane.Morand@unige.ch>
2012-08-09 23:25:47 +0200

    amelioration calcul bilan élève

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -9,26 +9,19 @@ class Competence < ActiveRecord::Base
   validates_presence_of :name
 #  validates :name, :presence => true
 
-  def bilan_note_eleve(user)
-    # on compte combien de chaque type de note
-    # pour chaque contexte
-    bilan = {}
+  
+  def bilan(eleve)
+    # On met le bilan de l'utilisateur dans un hash bilan[0] 
+    bilan = Hash.new(0)
+    bilan[0] = 0
+    bilan[1] = 0
+    bilan[2] = 0
+    bilan[3] = 0
     
-    
-    Context.where(:competence_id => self, :classroom_id => user.current_classroom).each do |context|
-    #user.current_classroom.competences.find(self).contexts.each do |context|
-      # pour chaque contexte récupérer la note de l'élève
-      # si note n'existe pas creer l'objet et donner 0 a note_eleve
-      note = context.notes.find_or_initialize_by_student_id(user)   do |n|
-        n.note_eleve = 0
-      end
-      note.save!
-      if bilan[note.note_eleve].nil?
-         bilan[note.note_eleve] = 1
-      else
-         bilan[note.note_eleve] += 1
-      end
+    Context.where(:competence_id => self, :classroom_id => eleve.current_classroom).each do |context|
+      bilan[context.note_eleve(eleve).note_eleve] += 1
     end
     bilan
   end
+  
 end
